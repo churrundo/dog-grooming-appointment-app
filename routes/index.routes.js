048c3ec7ba57477ddc946224965b6e7cb5f372f8
churrundo <churrundo@gmail.com>
2023-07-14 23:33:36 -0600

    move user profile route out of index and  into user router

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,12 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const isAdmin = require("../middleware/isAdmin");
-const isLoggedIn = require("../middleware/isLoggedIn");
-const Calendar = require("../models/Calendar.model");
-const Day = require("../models/Day.model");
-const createCalendar = require("../utils/generate-calendar");
-const User = require("../models/User.model");
-const Appointment = require("../models/Appointment.model");
 
 /* GET home page */
 router.get("/", (req, res, next) => {
@@ -19,24 +12,6 @@ router.get("/", (req, res, next) => {
   res.render("index");
 });
 
-router.get("/profile", isLoggedIn, async (req, res, next) => {
-  const user = req.session.currentUser;
-  if (user.role === "Admin") {
-    const calendarData = await Calendar.findOne({ userId: user._id })
-      .populate("userId")
-      .populate({
-        path: "days",
-        populate: {
-          path: "appointments",
-          model: "Appointment",
-        },
-      });
 
-    const calendar = createCalendar(calendarData);
-    res.render("profile", { user, rows: calendar.rows });
-  } else {
-    res.render("profile", { user });
-  }
-});
 
 module.exports = router;
